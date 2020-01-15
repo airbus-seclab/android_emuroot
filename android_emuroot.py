e2874929d34927cd5ba7baefce4d3f3b280b3efe
Mouad <abouhali@gmail.com>
2020-01-15 15:48:59 +0100

    mitigation print + import adb

--- android_emuroot.py
+++ android_emuroot.py
@@ -5,9 +5,16 @@ import logging
 import pygdbmi
 from pygdbmi.gdbcontroller import GdbController
 from pprint import pprint
-from adb.client import Client as AdbClient
 import threading
 import time
+
+
+
+try:
+    from adb.client import Client as AdbClient
+except:
+    from ppadb.client import Client as AdbClient
+      
 
 ##################### ADB functions #############################
 '''
@@ -193,7 +200,7 @@ class GDB_stub_controller(object):
         cur = stager_addr
         while True:
             parent_struct_addr = int(self.gdb.write("x/6xw %#x" % (cur + self.options.offset_to_comm - self.options.offset_to_parent))[1].get('payload').split('\\t')[1],16)
-            print parent_struct_addr
+            print(parent_struct_addr)
             parent_struct_name = self.gdb.write("x/s %#x" % (parent_struct_addr + self.options.offset_to_comm))[1].get('payload').split('\\t')[1]
             if (str(parent_struct_name) == r'\"adbd\"'):
                 adbd_cred_ptr = int(self.gdb.write("x/6xw %#x" % (parent_struct_addr + self.options.offset_to_comm - 4))[1].get('payload').split('\\t')[1],16)
