47aa1242b20aa7c13db434de2a577c68b681b282
Mouad <abouhali@gmail.com>
2020-01-16 11:43:22 +0100

    test python3 not yest completed bugs still exist

--- android_emuroot.py
+++ android_emuroot.py
@@ -26,7 +26,7 @@ def kernel_version():
     device = client.device(options.device)
     result = device.shell("uname -r")
     #result = result.encode('ascii','ignore')
-    logging.debug(" kernel_version() : result is %s", result)
+    logging.info(" kernel_version() : result is %s", result)
     result = result.split(".")
     ver = result[0]+'.'+result[1]
     ver = float(ver)
@@ -51,7 +51,7 @@ def kernel_version():
         ps_cmd = "ps -A"
         
     else :
-        logging.debug("Sorry. Android kernel version %s not supported yet", ver)
+        logging.info("Sorry. Android kernel version %s not supported yet", ver)
         raise NotImplementedError("Sorry. Android kernel version %s not supported yet", ver)
     return ver,offset_to_comm,offset_to_parent,offset_selinux,ps_cmd
 
@@ -63,10 +63,10 @@ def check_process_is_running(process, pscmd, device):
     device = client.device(device)
     ps = device.shell(pscmd)
     if process in ps:
-        logging.debug("[+] OK. %s is running" %(process))
+        logging.info("[+] OK. %s is running" %(process))
     else:
-        logging.debug("[+] NOK. It seems like %s is not running..." %(process))
-        logging.debug("[+] Android_Emuroot stopped")
+        logging.info("[+] NOK. It seems like %s is not running..." %(process))
+        logging.info("[+] Android_Emuroot stopped")
         exit(1)
 
 '''
@@ -78,7 +78,7 @@ def adb_stager_process(load):
     Adb connexion 
     TODO specify the device id
     '''
-    logging.debug("[+] Launch the stager process")
+    logging.info("[+] Launch the stager process")
     client = AdbClient(host="127.0.0.1", port=5037)
     device = client.device("emulator-5554")
 
@@ -94,7 +94,7 @@ def adb_stager_process(load):
 This function cleans the file system by removing the stager binaries created in adb_stager_process
 '''
 def stager_clean():
-    logging.debug("[+] Launch the stager process")
+    logging.info("[+] Launch the stager process")
     client = AdbClient(host="127.0.0.1", port=5037)
     device = client.device("emulator-5554")
 
@@ -107,21 +107,21 @@ def stager_clean():
 class GDB_stub_controller(object):
     def __init__(self, options):
         self.options = options
-        logging.debug(" [+] Start the GDB controller and attach it to the remote target")
-        logging.debug(" [+] GDB additional timeout value is %d" % int(options.timeout) )
+        logging.info(" [+] Start the GDB controller and attach it to the remote target")
+        logging.info(" [+] GDB additional timeout value is %d" % int(options.timeout) )
         self.gdb = GdbController(time_to_check_for_additional_output_sec=int(options.timeout))
         response = self.gdb.write("target remote :1234")
     def stop(self):
-        logging.debug(" [+] Detach and stop GDB controller")
+        logging.info(" [+] Detach and stop GDB controller")
         self.gdb.exit()
 
     def write(self, addr, val):
-        logging.debug(" [+] gdb.write adr: %#x value : %#x"%(addr,val))
+        logging.info(" [+] gdb.write adr: %#x value : %#x"%(addr,val))
         self.gdb.write("set *(unsigned int*) (%#x) = %#x" % (addr, val))
 
     def read(self, addr):
         r = self.gdb.write("x/6xw %#x" % addr)[1].get('payload').split('\\t')[1]
-        logging.debug(" [+] Gdb.read %s "%(r))
+        logging.info(" [+] Gdb.read %s "%(r))
         r = int(r,16)
         return r 
         #int(self.gdb.write("x/6xw %#x" % addr)[1].get('payload').split('\\t')[1],16)
@@ -130,8 +130,8 @@ class GDB_stub_controller(object):
     This function sets SELinux enforcement to permissive
     '''
     def disable_selinux(self):
-        logging.debug("[+] Disable SELinux")
-        logging.debug("[+] Offsets are  %s - %s - %s "%( hex(self.options.offset_selinux[0]),hex(self.options.offset_selinux[0]),hex(self.options.offset_selinux[0])))
+        logging.info("[+] Disable SELinux")
+        logging.info("[+] Offsets are  %s - %s - %s "%( hex(self.options.offset_selinux[0]),hex(self.options.offset_selinux[0]),hex(self.options.offset_selinux[0])))
 
         self.write(self.options.offset_selinux[0], 0)
         self.write(self.options.offset_selinux[1], 0)
@@ -141,7 +141,7 @@ class GDB_stub_controller(object):
     This function sets all capabilities of a task to 1
     '''
     def set_full_capabilities(self,cred_addr):
-        logging.debug("[+] Set full capabilities")
+        logging.info("[+] Set full capabilities")
         for ofs in [0x30, 0x34, 0x38, 0x3c, 0x40, 0x44]:
             self.write(cred_addr+ofs, 0xffffffff)
 
@@ -150,21 +150,21 @@ class GDB_stub_controller(object):
     @effective: if False, effective IDs are not modified 
     '''
     def set_root_ids(self, cred_addr, effective=True):
-        logging.debug("[+] Set root IDs")
+        logging.info("[+] Set root IDs")
         for ofs in [0x04, 0x08, 0x0c, 0x10, 0x1c, 0x20]: # uid, gid, suid,sgid, fsuid, fsgid
             self.write(cred_addr+ofs, 0x00000000)
         if effective:
             self.write(cred_addr+0x14, 0x00000000) # euid
             self.write(cred_addr+0x18, 0x00000000) # egid
         else:
-            logging.debug("[+] Note: effective ID have not been changed")
+            logging.info("[+] Note: effective ID have not been changed")
 
     '''
     This function returns the task_struct addr for a given process name
     '''
     def get_process_task_struct(self, process):
-        logging.debug(" [+] Get address aligned whose process name is: [%s]" % process)
-        logging.debug( "[+] GDB timeout is %d" % int(options.timeout) )
+        logging.info(" [+] Get address aligned whose process name is: [%s]" % process)
+        logging.info( "[+] GDB timeout is %d" % int(options.timeout) )
         response = self.gdb.write("find 0xc0000000, +0x40000000, \"%s\"" % process,
                                   raise_error_on_timeout=True, read_response=True,
                                   timeout_sec=int(options.timeout))
@@ -195,7 +195,7 @@ class GDB_stub_controller(object):
     This function returns the cred_struct address of adbd process from a given stager process
     '''
     def get_adbd_cred_struct(self, stager_addr):
-        logging.debug("[+] Search adbd task struct in the process hierarchy")
+        logging.info("[+] Search adbd task struct in the process hierarchy")
         adbd_cred_ptr = ""
         cur = stager_addr
         while True:
@@ -215,8 +215,8 @@ for a given process and patch its cred ID and capabilities
 @options: argparse namespace. Uses options.magic_name.
 '''
 def single_mode(options):
-    logging.debug("[+] Entering single function process name is %s " %(options.magic_name))
-    logging.debug("[+] Check if %s is running " %(options.magic_name))
+    logging.info("[+] Entering single function process name is %s " %(options.magic_name))
+    logging.info("[+] Check if %s is running " %(options.magic_name))
 
     # Check if the process is running
     check_process_is_running(options.magic_name, options.ps_cmd, options.device)
@@ -224,14 +224,14 @@ def single_mode(options):
     # Get task struct address
     gdbsc = GDB_stub_controller(options)
     magic = gdbsc.get_process_task_struct(options.magic_name)
-    logging.debug("[+] singel_mode(): process task struct of magic is %s "%(magic))
+    logging.info("[+] singel_mode(): process task struct of magic is %s "%(magic))
 
      # Replace the shell creds with id 0x0, keys 0x0, capabilities 0xffffffff
-    logging.debug("[+] single_mode(): Replace the process creds with id 0x0, keys 0x0, capabilities 0xffffffff")
+    logging.info("[+] single_mode(): Replace the process creds with id 0x0, keys 0x0, capabilities 0xffffffff")
     print ("magic is %s " %(hex(magic)))
     print (" magic cred is at %s "%hex(magic+options.offset_to_comm-8) )
     magic_cred_ptr = gdbsc.read(magic+options.offset_to_comm-8)
-    logging.debug("[+] single_mode(): magic_cred_ptr is %s "%hex(magic_cred_ptr))
+    logging.info("[+] single_mode(): magic_cred_ptr is %s "%hex(magic_cred_ptr))
     gdbsc.set_root_ids(magic_cred_ptr)
     gdbsc.set_full_capabilities(magic_cred_ptr)
 
@@ -243,7 +243,7 @@ This function install a sh with suid root on the file system
 @options: argparse namespace. Uses options.path.
 '''
 def setuid_mode(options):
-    logging.debug("[+] Rooting with Android Emuroot via a setuid binary...")
+    logging.info("[+] Rooting with Android Emuroot via a setuid binary...")
 
     script = """'#!/bin/bash
 cp /system/bin/sh /data/local/tmp/{0}
@@ -280,8 +280,8 @@ This function elevates the privileges of adbd process
 @options: argparse namespace. Uses options.stealth.
 '''
 def adbd_mode(options):
-    logging.debug("adbd mode is chosen")
-    logging.debug("[+] Rooting with Android Emuroot via adbd...")
+    logging.info("adbd mode is chosen")
+    logging.info("[+] Rooting with Android Emuroot via adbd...")
 
     script = """'#!/bin/bash
 cp /system/bin/sh /data/local/tmp/probe
@@ -344,8 +344,8 @@ if __name__ == '__main__':
         parser.error("Too few arguments")
 
     # set logging params
-    loglevel = 70 - (10*options.verbose) if options.verbose > 0 else 0
-    logging.basicConfig(level=loglevel, format='%(asctime)s %(levelname)s: %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
+    #loglevel = 70 - (10*options.verbose) if options.verbose > 0 else 0
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 
     # pin down android kernel version
     options.version, options.offset_to_comm, options.offset_to_parent , options.offset_selinux, options.ps_cmd = kernel_version()
